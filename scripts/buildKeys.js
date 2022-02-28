d3a98818369ad929fb40cce39ef6ab9b81e813ce
Thagana <samuelmthwa79@gmail>
2022-02-28 20:10:53 +0200

    files

--- scripts/buildKeys.js
+++ scripts/buildKeys.js
@@ -6,6 +6,5 @@ file.APPLICATION_URL = "https://api.ultimatenews.com";
     
 fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
   if (err) return console.log(err);
-  console.log(JSON.stringify(file));
   console.log('writing to ' + fileName);
 });
